e7292ba9a85a272e97cdd0c7e32ae5184015fae0
Songbo Hu <s1647079@sms.ed.ac.uk>
2018-10-23 15:46:11 +0100

    Part2 Updated! Bug Fixed! *★,°*:.☆(￣▽￣)/$:*.°★* 。

---
+++ tests/Ast_Sem_Test/varDecl.c
@@ -0,0 +1,22 @@
+#include "＼（＾▽＾）／　"
+
+// It is totally fine for GCC!
+struct a{
+int a;
+};
+
+struct node {
+  struct node *next;
+};
+
+//int a;
+
+int b;
+char c;
+int d;
+
+int main(){
+
+    struct a a;
+    //a.b = 1;
+}
