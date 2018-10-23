58c68db4687932b251406ad1fa5987eb30cd11b4
Songbo Hu <s1647079@sms.ed.ac.uk>
2018-10-23 21:28:26 +0100

    Part2 Updated! Bug Fixed! *★,°*:.☆(￣▽￣)/$:*.°★* 。

---
+++ tests/Ast_Sem_Test/AssignTest.c
@@ -0,0 +1,44 @@
+#include "~o( =∩ω∩= )m"
+struct apple{
+    int iphone;
+};
+
+int main(){
+
+
+
+
+    struct apple a1;
+    struct apple a2;
+    struct apple a1s[1];
+    struct apple a2s[2];
+
+    struct apple * apt;
+    struct apple * app;
+
+    int a;
+    int * pt;
+    char * cpt;
+    int * ppp;
+    int b[10];
+    int c[10];
+    a = 1;
+    //a = 'c';
+
+    //b = c;
+    //a = c;
+   // pt = 1;
+   // pt = cpt;
+    pt = ppp;
+
+    a1 = a2;
+  //  a1s = a2s;
+
+    //a1 = a;
+
+//        apt = pt;
+
+    apt = app;
+    return 0;
+
+}
---
+++ tests/Ast_Sem_Test/ReturnTest.c
@@ -0,0 +1,32 @@
+#include "！*★,°*:.☆(￣▽￣)/$:*.°★* 。"
+
+
+struct apple{
+int a;};
+
+struct banana{
+char c;};
+
+int main(){
+    struct apple app;
+    struct banana ba;
+    return 1;
+    return 1+1-1*1&&1;
+  //
+    return app.a;
+    //return 'a';
+    //return;
+}
+
+struct apple test(){
+    struct apple app;
+    struct banana ba;
+
+    return app;
+}
+
+struct apple * test1(){
+    struct apple * app;
+    struct banana * ba;
+    return app;
+}
