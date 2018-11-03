d17d0f3c613096392bc5a7c322a0084281c9e029
Songbo Hu <s1647079@sms.ed.ac.uk>
2018-11-03 17:05:08 +0000

    Part 3 Visitor ✧୧(๑=̴̀⌄=̴́๑)૭✧!

--- tests/Gen_Test/Offset.c
+++ tests/Gen_Test/Offset.c
@@ -18,7 +18,7 @@ struct banana{
 int a;
 int b;
 
-int test(int a, struct apple iphone){
+int test(int a, struct apple iphone, int q, int w, int e, int r){
     int b;
     struct apple c;
 
@@ -37,7 +37,13 @@ int main(){
     int a;
     int b;
     int c;
-    //test(1);
+    int dd[10];
+    struct apple * iphone;
+    struct apple iphones[10];
+    struct apple aiphone;
+    struct banana ba;
+    struct banana banas[10];
+        //test(1);
     return 0;
 
 }
