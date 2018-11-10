ce92aedd5e405d4118a8498d34d9ae9b99e2f490
Songbo Hu <s1647079@sms.ed.ac.uk>
2018-11-10 20:08:37 +0000

    Part 3  ✧୧(๑=̴̀⌄=̴́๑)૭✧!

--- tests/Gen_Test/VarDeclTest.c
+++ tests/Gen_Test/VarDeclTest.c
@@ -14,17 +14,36 @@ struct apple apps[10086];
 
 char chars[10];
 
+char ch;
+
 int main(){
 
 struct apple ap_stack;
 
-struct apple apps[10086];
+struct apple apps1[10086];
 
-char chars[10];
+char chars1[10];
 
 int * pt[10];
+char ch1;
 
+a;
 
+app;
+
+apps;
+
+chars;
+
+ap_stack;
+
+apps1;
+chars1;
+pt;
+
+ch;
+
+ch1;
 
 return 10;
 
